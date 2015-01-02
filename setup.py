89e54ccc70f408bcdeb9ce03c97742ec74e3125a
JamshedVesuna <jamshed.vesuna@gmail.com>
2015-01-02 14:22:15 -0800

    Updated version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'cronos',
   packages = ['cronos'],
-  version = '0.0.1',
+  version = '0.0.2',
   description = 'Python module for safely and cleanly using sensitive data in code',
   author = 'Jamshed Vesuna',
   author_email = 'jvesuna314@gmail.com',
   url = 'https://github.com/JamshedVesuna/cronos',
-  download_url = 'https://github.com/JamshedVesuna/cronos/archive/v0.0.1.tar.gz',
+  download_url = 'https://github.com/JamshedVesuna/cronos/archive/v0.0.2.tar.gz',
   keywords = ['cronos','sensitive', 'encrypt', 'data'],
   classifiers = [],
 )
