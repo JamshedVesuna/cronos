ecba22e065dd866a069a18b93082662f584d1fdf
Jamshed Vesuna <jamshed@robinhood.com>
2016-05-16 14:56:37 -0700

    Bump release version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'cronos',
   packages = ['cronos'],
-  version = '0.0.4',
+  version = '0.0.5',
   install_requires = ['simple-crypt'],
   description = 'Python module for safely and cleanly using sensitive data in code',
   author = 'Jamshed Vesuna',
   author_email = 'jvesuna314@gmail.com',
   url = 'https://github.com/JamshedVesuna/cronos',
-  download_url = 'https://github.com/JamshedVesuna/cronos/archive/0.0.4.tar.gz',
+  download_url = 'https://github.com/JamshedVesuna/cronos/archive/0.0.5.tar.gz',
   keywords = ['cronos','sensitive', 'encrypt', 'data'],
   classifiers = [],
 )
